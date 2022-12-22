5d6e304e6f8821647c8a7721b66fe327078b6566
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-12-22 18:35:00 +0100

    Set Up

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Utility</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css%22%3E">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css%22%3E">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+
+<body>
+
+    <div id="app">
+        <h1>TITLE</h1>
+    </div>
+
+    <!-- javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
